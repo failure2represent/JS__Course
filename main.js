61504c285ee2328ca33a9c793d226346fef1ef06
valerii6 <markvalonski@gmail.com>
2024-09-10 17:19:14 +0300

    homework4 for ls4

--- main.js
+++ main.js
@@ -1,46 +1,21 @@
-let userBirthYear = prompt("Please enter your birth year (YYYY):");
-let userAge = 2024 - userBirthYear
-    if(!userBirthYear){
-        console.log("Too bad you didn't want to specify the year of birth (╥﹏╥)")
-    }
-    else if(isNaN(userBirthYear)){
-        console.log("Too bad you didn't enter the year correctly (╥﹏╥)")
-    }
-    else console.log(`Your age is ${userAge}`)
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
 
-let userCity = prompt("Please enter the city where you live:");
-    if(userCity){
-        userCity = userCity.toLowerCase();
-        if(userCity === "kyev"){
-            console.log("You live in the capital of Ukraine!");
-        }
-        else if(userCity === "washington"){
-            console.log("You live in the capital of the USA!");
-        }
-        else if (userCity === "london"){
-            console.log("You live in the capital of Great Britain!");
-        }
-        else console.log(`You live in ${userCity.charAt(0).toUpperCase() + userCity.slice(1)}`)
-    }
-    else console.log("Too bad you didn't want to specify your city (╥﹏╥)")
-    
-let userSportKind = prompt("Please enter your favourite kind of sport:");
-    if(userSportKind){
-        userSportKind = userSportKind.toLowerCase();
-        switch(userSportKind){
-            case "boxing":
-                console.log("Wow! You wanna be Mike Tyson one day ?");
-                break;
-            case "hockey":
-                console.log("Whoa! You want be Wayne Gretzky one day ?");
-                break;
-            case "soccer":
-                console.log("Huh, you wanna play soccer instead of rugby ?  (ㆆ _ ㆆ)");
-                break;
-            default:
-                console.log(`Your favourite sport is ${userSportKind.charAt(0).toUpperCase() + userSportKind.slice(1)}`)
-                break;
-        }
-    }
-    else console.log("Too bad you didn't want to specify your favourite kind of sport (╥﹏╥)")
-    
+
+switch(true){
+    case numOrStr === null:
+        console.log('ви скасували');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+    case isNaN(+numOrStr):
+        console.log('number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+        break;
+}
+
+
+
