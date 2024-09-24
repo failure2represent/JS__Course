9670de1a7999c444daff9f669e6f72ac3d4b4f91
valerii6 <markvalonski@gmail.com>
2024-09-24 17:51:41 +0200

    hw2 toMultyply

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 
-
-function toCount(){
-    let count = 0;
-
-    return function(value){
-        count += value
-        return count
+function multyply(firstID){{
+        return function (secondID){
+            return firstID*secondID
+        }
     }
 }
+const firstNumber = prompt("Enter your multyplyer")
+const secondNumber = prompt("Enter your number to multyply")
 
-const sum = toCount();
-
-console.log(sum(4))
-console.log(sum(6))
-console.log(sum(10))
-console.log(sum(7))
+console.log(multyply(firstNumber)(secondNumber))
