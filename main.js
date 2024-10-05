d86f7ab486bd5bedae9925b97f6b0af767d1789f
valerii6 <markvalonski@gmail.com>
2024-10-05 17:32:48 +0200

    ver2

--- main.js
+++ main.js
@@ -1,25 +1,9 @@
 const contactInfo = {
     contactList : [
-        {
-        name : "John",
-        email : "j@j.com",
-        phone : "123-123-1234"
-    },
-    {
-        name : "John",
-        email : "jj@j.com",
-        phone : "123-123-2234"
-    },
-    {
-        name : "Jane",
-        email : "d@j.com",
-        phone : "123-123-1334"
-    },
-    {
-        name : "Jim",
-        email : "k@j.com",
-        phone : "123-123-1434"
-    }
+        {name : "John", email : "j@j.com", phone : "123-123-1234"},
+        {name : "John", email : "jj@j.com", phone : "123-123-2234"},
+        {name : "Jane", email : "d@j.com", phone : "123-123-1334"},
+        {name : "Jim", email : "k@j.com", phone : "123-123-1434"}
 ],
     getContactName: function(name) {
         const results = this.contactList.filter(contact => contact.name === name); 
@@ -44,5 +28,5 @@ console.log(defaultContactList, "==> default contact list");
 contactInfo.addNewContact("Tim", "t@t.com", "123-123-1534");
 console.log(contactInfo.contactList,); 
 contactInfo.addNewContact("John", "jh2@t.com", "123-133-6534");
-console.log(contactInfo.contactList,); 
+console.log(contactInfo.contactList,);
 console.log(contactInfo.getContactName("John"));
